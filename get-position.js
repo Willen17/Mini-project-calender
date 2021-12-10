2c45822cdc4130c51e729ead51f89a1ec0bbf2a5
William Saar <williamsaar@outlook.com>
2021-12-10 18:05:21 +0100

    Added weather API

--- get-position.js
+++ get-position.js
@@ -1,6 +1,7 @@
 
 function initGetPosition() {
     addGetPositionEventlisteners();
+    fetchWeatherInfoForCurrentDay()
 }
 let userCordinates = {latitude:'', longitude:''};
 
@@ -19,4 +20,15 @@ function getLocation() {
 function showPosition(position) {
     userCordinates.latitude = position.coords.latitude;
     userCordinates.longitude = position.coords.longitude;
+}
+
+async function fetchWeatherInfoForCurrentDay() {
+  try {
+      const response = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + userCordinates.latitude +'&lon=' + userCordinates.longitude + '&units=metric&exclude=minutely,hourly,daily,alerts&appid=9cad7f8da0da8d85ed29aa07aa5a3591')
+      const data = await response.json();
+      console.log(data);
+      // renderAllHolidays(data);
+  } catch (error) {
+      console.error(error);
+  }
 }
