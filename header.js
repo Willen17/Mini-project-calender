54a5594b8ad7cf5770e513ce5ffd3e6fb8e8e63d
Love Lanai <love.lanai@medieinstitutet.se>
2021-12-21 17:03:28 +0100

    fixed buttons

--- header.js
+++ header.js
@@ -11,6 +11,8 @@ let isViewOnMobileDevices = false;
  */
 function initheader() {
     addheaderEventListeners();
+    showTodoListOnMobileDevice();
+
 }
 
 /**
@@ -36,15 +38,23 @@ function showTodoListOnMobileDevice() {
 
     const asideSection = document.getElementById('aside-section');
     const todoMobileBtn = document.getElementById('todo-mobile-button');
+    
+    const hideTodoButton = document.getElementById('todo-mobile-not-a-button');
+    const hideCalendarButton = document.getElementById('calendar-mobile-not-a-button');
+    const calendarMobileBtn = document.getElementById('calendar-mobile-button');
 
     if(asideSection.style.display === 'flex') {
         asideSection.style.display = 'none';
-        todoMobileBtn.style.color = null;
+
         isViewOnMobileDevices = null;
+        
     } else {
         asideSection.style.display = 'flex';
-        todoMobileBtn.style.color = 'black';
         isViewOnMobileDevices = 'calendar';
+        todoMobileBtn.style.display = 'none';
+        hideTodoButton.style.display='unset'
+        calendarMobileBtn.style.display = 'unset';
+        hideCalendarButton.style.display='none'
     }
 }
 
@@ -52,13 +62,18 @@ function showTodoListOnMobileDevice() {
  * shows the calendar section on mobile devices
  */
 function showCalendarOnMobileDevice() {
-    if(isViewOnMobileDevices === 'calendar') {
+    if(isViewOnMobileDevices === 'calendar' || isViewOnMobileDevices === false) {
         showTodoListOnMobileDevice();
     }
-
+    
     const calendarSection = document.getElementById('calendar-section');
     const calendarMobileBtn = document.getElementById('calendar-mobile-button');
 
+    const hideCalendarButton = document.getElementById('calendar-mobile-not-a-button');
+    const todoMobileBtn = document.getElementById('todo-mobile-button');
+    const hideTodoButton = document.getElementById('todo-mobile-not-a-button');
+
+    
     if(calendarSection.style.display === 'unset') {
         calendarSection.style.display = null;
         calendarMobileBtn.style.color = null;
@@ -68,15 +83,34 @@ function showCalendarOnMobileDevice() {
         calendarSection.style.width = '100%';
         calendarMobileBtn.style.color = 'black';
         isViewOnMobileDevices = 'todo';
+        calendarMobileBtn.style.display = 'none';
+        hideCalendarButton.style.display='unset'
+        todoMobileBtn.style.display = 'unset'
+        hideTodoButton.style.display='none'
     }
 }
 
 function resetWindow() {
     const asideSection = document.getElementById('aside-section');
     const calendarSection = document.getElementById('calendar-section');
+    
+    const todoMobileBtn = document.getElementById('todo-mobile-button');
+    const hideTodoButton = document.getElementById('todo-mobile-not-a-button');
+    const calendarMobileBtn = document.getElementById('calendar-mobile-button');
+    const hideCalendarButton = document.getElementById('calendar-mobile-not-a-button');
+
 
     if(window.innerWidth > 768) {
         asideSection.style.display = 'flex';
         calendarSection.style.display = null;
+        isViewOnMobileDevices = false;
+
+
+
+        todoMobileBtn.style.display = 'none';
+        hideTodoButton.style.display='unset'
+        calendarMobileBtn.style.display = 'unset';
+        hideCalendarButton.style.display='none'
     }
 }
+
